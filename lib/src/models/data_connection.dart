63e9f622fe644d0845eccb7afb68af2cb0ce8098
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-02-05 17:00:53 -0600

    Se documenta el model

--- lib/src/models/data_connection.dart
+++ lib/src/models/data_connection.dart
@@ -8,12 +8,20 @@ class DataConnection {
   ///RFC of Organism
   final String rfc;
 
+  /// Constructor
   DataConnection({
     required this.appName,
     required this.urlApi,
     required this.rfc,
   });
 
+  /// [copyWith] is a method that returns a new instance of the `DataConnection`
+  /// class with the same data as the current instance, but with the data
+  /// provided in the arguments.
+  ///
+  /// [urlApi] is the url of the api.
+  /// [rfc] is the rfc of the organism.
+  /// [appName] is the name of the app. This is not necessary to provide it.
   DataConnection copyWith({String? urlApi, String? rfc}) => DataConnection(
         appName: appName,
         urlApi: urlApi ?? this.urlApi,
