1d14c081e1994efdeaee36da9d56580a7a88a944
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-02-05 17:01:12 -0600

    Se documenta el model

--- lib/src/models/dio_action_data.dart
+++ lib/src/models/dio_action_data.dart
@@ -1,32 +1,16 @@
 import 'package:api_communications/src/enums/http_method.dart';
 import 'package:dio/dio.dart';
 
-///This class called `DioActionData` that represents data that can be used to
-///make a request using the `Dio` library.
+/// The class [DioActionData] is a class used to make requests over HTTP with
+/// [Dio]. It holds information needed for the request such as the [dio] instance,
+/// the [path] of the resource being requested, the [httpMethod] of the request
+/// (e.g. GET, POST), and optional [headers], [dataToSend], and progress callbacks
+/// for both sending and receiving.
 ///
-/// The `DioActionData` class has six instance variables:
-///
-/// `dio` an instance of the `Dio` class that will be used to make the request.
-///
-/// `path` a `String` representing the path of the resource being requested.
-///
-/// `httpMethod` an instance of the `HttpMethod` enum that represents the HTTP
-/// method to use for the request (e.g. GET, POST.).
-///
-/// `headers` a `Map` containing headers. This is optional and can be null.
-///
-/// `dataToSend` a `Map` containing data to send with the request. This is
-/// optional and can be null.
-///
-/// `onSendProgress` a callback function that will be called with progress
-/// updates when sending the request. This is optional and can be null.
-///
-/// `onReceiveProgress` a callback function that will be called with progress
-/// updates when receiving the response. This is optional and can be null.
-///
-/// The class has a single constructor that initializes these instance
-/// variables. The required keyword indicates that these arguments are required
-///  and must be provided when creating an instance of the `DioActionData` class.
+/// It also holds the [baseUrl], [rfc], and [appName] values, which may be used
+/// in constructing the full URL for the request. All of these parameters are
+/// required when constructing an instance of this class, except for the headers,
+/// data to send, and progress callbacks, which are optional and can be null.
 class DioActionData {
   /// `dio` an instance of the `Dio` class that will be used to make the request.
   final Dio dio;
