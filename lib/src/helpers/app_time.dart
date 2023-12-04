c066725392151c42e23bbce186e771f521c3a19d
Oscar Isaac Tinajero Maldonado <tinajero117.oscar@gmail.com>
2023-12-04 16:11:50 -0600

    Update app_time.dart
    suma de dias

--- lib/src/helpers/app_time.dart
+++ lib/src/helpers/app_time.dart
@@ -3,4 +3,11 @@ class AppTime {
   /// date and time in the format `YYYYMMDD HH:MM:SS.SSS`.
   static String dateTimeNow() =>
       (DateTime.now()).toString().replaceAll("-", "").substring(0, 21);
+
+  static String dateTimeNowPlusDays({int days = 35}) {
+    final now = DateTime.now();
+    final futureDate = now.add(Duration(days: days));
+
+    return futureDate.toString().replaceAll("-", "").substring(0, 8);
+  }
 }
