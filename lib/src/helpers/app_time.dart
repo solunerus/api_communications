8f6eddb5d5ab2ee871221f09c6e5682c58d29849
Oscar Isaac Tinajero Maldonado <tinajero117.oscar@gmail.com>
2024-01-02 14:17:25 -0600

    Update app_time.dart

--- lib/src/helpers/app_time.dart
+++ lib/src/helpers/app_time.dart
@@ -1,8 +1,8 @@
 class AppTime {
   /// [dateTimeNow] is a method that returns a `String` containing the current
   /// date and time in the format `YYYYMMDD HH:MM:SS.SSS`.
-  static String dateTimeNow() =>
-      (DateTime.now()).toString().replaceAll("-", "").substring(0, 21);
+  static String dateTimeNow({int endNum = 21}) =>
+      (DateTime.now()).toString().replaceAll("-", "").substring(0, endNum);
 
   static String dateTimeNowPlusDays({int days = 35}) {
     final now = DateTime.now();
